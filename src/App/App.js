8ef07b0a6e111ae5878529c7db0eb5209b0e8b9f
JP Carey <jpcarey4@gmail.com>
2021-01-13 11:34:14 -0700

    make App a class Component

--- src/App/App.js
+++ src/App/App.js
@@ -4,59 +4,68 @@ import Name from '../Name/Name'
 import Question from '../Question/Question'
 import Result from '../Result/Result'
 import Error from '../Error/Error'
+import React, { Component } from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-const App = () => {
-  return (
-    <main>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={() => {
-            return (
-              <Intro />
-            )
-          }}
-        />
-        <Route 
-          exact
-          path="/your-name"
-          render={() => {
-            return (
-              <Name />
-            )
-          }}
-        />
-        <Route 
-          exact
-          path="/question"
-          render={() => {
-            return (
-              <Question />
-            )
-          }}
-        />
-        <Route 
-          exact
-          path="/result"
-          render={() => {
-            return (
-              <Result />
-            )
-          }}
-        />
-        <Route 
-          path="/"
-          render={() => {
-            return (
-              <Error />
-            )
-          }}
-        />
-      </Switch>
-    </main>
-  )
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      
+    }
+  }
+  render() {
+    return (
+      <main>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <Intro />
+              )
+            }}
+          />
+          <Route 
+            exact
+            path="/your-name"
+            render={() => {
+              return (
+                <Name />
+              )
+            }}
+          />
+          <Route 
+            exact
+            path="/question"
+            render={() => {
+              return (
+                <Question />
+              )
+            }}
+          />
+          <Route 
+            exact
+            path="/result"
+            render={() => {
+              return (
+                <Result />
+              )
+            }}
+          />
+          <Route 
+            path="/"
+            render={() => {
+              return (
+                <Error />
+              )
+            }}
+          />
+        </Switch>
+      </main>
+    )
+  }
 }
 
 export default App

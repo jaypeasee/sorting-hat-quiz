52cfcccda1788480463479d855c457cb20811fc3
JP Carey <jpcarey4@gmail.com>
2021-01-13 11:09:19 -0700

    setup app with router to each component

--- src/App/App.js
+++ src/App/App.js
@@ -1,8 +1,106 @@
 import './App.scss'
+import Intro from '../Intro/Intro'
+import Name from '../Name/Name'
+import Question from '../Question/Question'
+import Result from '../Result/Result'
+import Error from '../Error/Error'
+import { Route, Switch } from 'react-router-dom'
 
-function App() {
+const App = () => {
   return (
-    <h1>Hello</h1>
+    <main>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => {
+            return (
+              <Intro />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/your-name"
+          render={() => {
+            return (
+              <Name />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-1"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-2"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-3"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-4"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-5"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/question-6"
+          render={() => {
+            return (
+              <Question />
+            )
+          }}
+        />
+        <Route 
+          exact
+          path="/result"
+          render={() => {
+            return (
+              <Result />
+            )
+          }}
+        />
+        <Route 
+          path="/"
+          render={() => {
+            return (
+              <Error />
+            )
+          }}
+        />
+      </Switch>
+    </main>
   )
 }
 

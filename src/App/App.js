1cbad3dd0ec8920957f05cf31e7a2f10c0b035b1
JP Carey <jpcarey4@gmail.com>
2021-01-13 11:50:31 -0700

    make loose structure of Name, Question Components

--- src/App/App.js
+++ src/App/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   }
   render() {
     return (
-      <main>
+      <main className="app">
         <Switch>
           <Route
             exact
--- src/Name/Name.js
+++ src/Name/Name.js
@@ -5,9 +5,11 @@ import { Link } from 'react-router-dom'
 const Name = () => {
     return (
         <form className="name-form">
+            <h1>What's Your Name?</h1>
             <input
                 type="text"
                 name="name"
+                placeholder="Your Name"
             />
             <Link
                 to="/question"

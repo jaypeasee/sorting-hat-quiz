3d8bba627605ea2a69184c4fb3764c701c449b08
JP Carey <jpcarey4@gmail.com>
2021-01-13 12:57:45 -0700

    make Name a class component

--- src/Name/Name.js
+++ src/Name/Name.js
@@ -1,23 +1,32 @@
 import './Name.scss'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+class Name extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            nameInput: ""
+        }
+    }
 
-const Name = () => {
-    return (
-        <form className="name-form">
-            <h1>What's Your Name?</h1>
-            <input
-                type="text"
-                name="name"
-                placeholder="Your Name"
-            />
-            <Link
-                to="/question"
-            >
-                <button>Enroll!</button>
-            </Link>
-        </form>
-    )
+    render() {
+        return (
+            <form className="name-form">
+                <h1>What's Your Name?</h1>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Your Name"
+                />
+                <Link
+                    to="/question"
+                >
+                    <button>Enroll!</button>
+                </Link>
+            </form>
+        )
+    }
 }
 
 export default Name

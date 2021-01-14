a7bcc118c0e21dac1368107608553f8abfa4d7bd
JP Carey <jpcarey4@gmail.com>
2021-01-14 15:44:20 -0700

    style the error component no matter where it comes from

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -1,10 +1,17 @@
 import './Error.scss'
+import { Link } from 'react-router-dom'
 
 const Error = (props) => {
     return (
         <section className="error-section">
             {!props.errorMessage && <h1>Oops! This page does not exist.</h1>}
             {props.errorMessage && <h1>{props.errorMessage}</h1>}
+            <Link
+                to="/"
+                className="error-btn-anchor"
+            >
+                <button className="error-btn">Return To The Sorting Hat</button>
+            </Link>
         </section>
     )
 }

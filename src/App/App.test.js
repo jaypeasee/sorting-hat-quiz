6e66fc7feae3255a0f2aab7b0d037a08ac0ef1d0
JP Carey <jpcarey4@gmail.com>
2021-01-15 13:28:11 -0700

    setup App tests

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,8 +1,24 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import { render, screen } from '@testing-library/react';
+import { getAllCharacters, getHogwartsHouses } from '../utilities'
+import { mockRavenclawData, mockCharacterData } from '../mockData'
+import { screen, render, waitFor } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import userEvent from '@testing-library/user-event'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+jest.mock('../utilities')
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+
+describe("App", () => {
+  const history = createMemoryHistory()
+
+  beforeEach(() => {
+    getHogwartsHouses.mockResolvedValue()
+    render (
+      <Router history={ history }>
+        <App></App>
+      </Router>
+    )
+  })
+})

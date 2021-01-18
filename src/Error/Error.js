f04bf99c06c3862a4c70bd19432a4a67111db2dd
JP Carey <jpcarey4@gmail.com>
2021-01-18 09:21:12 -0700

    cleanup code in Error folder

--- src/Error/Error.js
+++ src/Error/Error.js
@@ -2,7 +2,7 @@ import './Error.scss'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types';
 
-const Error = ({errorMessage}) => {
+const Error = ({ errorMessage }) => {
     return (
         <section className="error-section">
             <h1>{errorMessage}</h1>
